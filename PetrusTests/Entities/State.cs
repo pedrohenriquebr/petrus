6049da9d4816a835b3913cc37eec26723bef4c0a
pedrohenriquebr <pedrohenriquebraga735@gmail.com>
2022-04-13 09:21:32 -0300

    Add State model class to Unit Test project #15

---
+++ PetrusTests/Entities/State.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrusTests.Entities
+{
+
+    public class State
+    {
+        [JsonProperty("nome")]
+        public string Name { get; set; }
+    }
+
+}
